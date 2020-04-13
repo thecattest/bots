88a609da7d697b011e26846cbb37627c033a2949
vodopyanov ilya <vodopyanov999@gmail.com>
2020-04-13 18:30:56 +0300

    update weather bot

--- telegram/weather_skip.py
+++ telegram/weather_skip.py
@@ -18,14 +18,7 @@ def start(update, context):
         "Вы можете пропустить этот вопрос, отправив команду /skip"
     )
 
-    # Число-ключ в словаре states —
-    # втором параметре ConversationHandler'а.
     return 1
-    # Оно указывает, что дальше на сообщения от этого пользователя
-    # должен отвечать обработчик states[1].
-    # До этого момента обработчиков текстовых сообщений
-    # для этого пользователя не существовало,
-    # поэтому текстовые сообщения игнорировались.
 
 
 def skip(update, context):
@@ -34,23 +27,15 @@ def skip(update, context):
 
 
 def first_response(update, context):
-    # Это ответ на первый вопрос.
-    # Мы можем использовать его во втором вопросе.
     locality = update.message.text
     update.message.reply_text(
         "Какая погода в городе {locality}?".format(**locals()))
-    # Следующее текстовое сообщение будет обработано
-    # обработчиком states[2]
     return 2
 
 
 def second_response(update, context):
-    # Ответ на второй вопрос.
-    # Мы можем его сохранить в базе данных или переслать куда-либо.
-    weather = update.message.text
     update.message.reply_text("Спасибо за участие в опросе! Всего доброго!")
-    return ConversationHandler.END  # Константа, означающая конец диалога.
-    # Все обработчики из states и fallbacks становятся неактивными.
+    return ConversationHandler.END
 
 
 def main():
@@ -58,20 +43,13 @@ def main():
     dp = updater.dispatcher
 
     conv_handler = ConversationHandler(
-        # Точка входа в диалог.
-        # В данном случае — команда /start. Она задаёт первый вопрос.
         entry_points=[CommandHandler('start', start)],
 
-        # Состояние внутри диалога.
-        # Вариант с двумя обработчиками, фильтрующими текстовые сообщения.
         states={
-            # Функция читает ответ на первый вопрос и задаёт второй.
             1: [MessageHandler(Filters.regex('^[^/]'), first_response), CommandHandler('skip', skip)],
-            # Функция читает ответ на второй вопрос и завершает диалог.
             2: [MessageHandler(Filters.regex('^[^/]'), second_response)]
         },
 
-        # Точка прерывания диалога. В данном случае — команда /stop.
         fallbacks=[CommandHandler('stop', stop)]
     )
     dp.add_handler(conv_handler)
