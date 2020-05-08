7dff9df64aef5766852bb75ef1f265c6ba025c39
vodopyanov ilya <vodopyanov999@gmail.com>
2020-05-08 10:08:28 +0300

    wall posts request

---
+++ vk/1wall_posts.py
@@ -0,0 +1,26 @@
+import vk_api
+import datetime
+
+
+def main():
+    login, password = LOGIN, PASSWORD
+    vk_session = vk_api.VkApi(login, password)
+    try:
+        vk_session.auth(token_only=True)
+    except vk_api.AuthError as error_msg:
+        print(error_msg)
+        return
+    vk = vk_session.get_api()
+
+    response = vk.wall.get(count=5, filter='owner')
+    if response['items']:
+        for note in response['items']:
+            timestamp = note['date']
+            text = note['text']
+            date_obj = datetime.datetime.fromtimestamp(timestamp)
+            date = date_obj.strftime('date: {%Y-%m-%d}, time: {%H:%M:%S}')
+            print(f"{{'{text}'}};\n{date}\n")
+
+
+if __name__ == '__main__':
+    main()
